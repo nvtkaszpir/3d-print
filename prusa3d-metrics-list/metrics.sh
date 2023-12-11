61316d55917f5d03a38a61a4361cc50f717d3b5b
Michał Sochoń <kaszpir@gmail.com>
2023-12-12 00:20:44 +0100

    Fix typos in output

--- prusa3d-metrics-list/metrics.sh
+++ prusa3d-metrics-list/metrics.sh
@@ -10,7 +10,7 @@ date=$(date --utc)
 # print all metrics available in Prusa-Firmware-Buddy
 echo "Name,Type,File" > metrics.csv
 grep -r -E 'METRIC\((.*)"([^"]+)"(.*)METRIC_VALUE_([A-Z_]+)' \
-| sed -E 's/(.*):.*METRIC\((.*)"([^"]+)"(.*)METRIC_VALUE_([A-Z_]+).*/\3,\5,\1 /' \
+| sed -E 's/(.*):.*METRIC\((.*)"([^"]+)"(.*)METRIC_VALUE_([A-Z_]+).*/\3,\5,\1/' \
 | sed 's/Prusa-Firmware-Buddy\///g' \
 | sort | uniq >> metrics.csv
 echo "# generated at ${date},# PrusaFirmwareBuddy commit=${commit},# script source https://github.com/nvtkaszpir/3d-print" >> metrics.csv
@@ -19,7 +19,7 @@ echo "# generated at ${date},# PrusaFirmwareBuddy commit=${commit},# script sour
 # add gcode prefixes
 echo ";generated at ${date}, PrusaFirmwareBuddy commit=${commit}, script source https://github.com/nvtkaszpir/3d-print" > metrics.gcode
 grep -r -E 'METRIC\((.*)"([^"]+)"(.*)METRIC_VALUE_([A-Z_]+)' \
-| sed -E 's/(.*):.*METRIC\((.*)"([^"]+)"(.*)METRIC_VALUE_([A-Z_]+).*/M331 \3 ;metric type \5 - \1 /' \
+| sed -E 's/(.*):.*METRIC\((.*)"([^"]+)"(.*)METRIC_VALUE_([A-Z_]+).*/M331 \3 ;metric type \5 - \1/' \
 | sed 's/Prusa-Firmware-Buddy\///g' \
 | sort | uniq >> metrics.gcode
 
